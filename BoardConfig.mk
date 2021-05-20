488bfc9d45db95f10596c79479a16fa35c293689
Tata Lee <67614323+macukex1991@users.noreply.github.com>
2021-05-20 20:20:46 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,6 +130,6 @@ PLATFORM_VERSION := 16.1.0
 TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # PBRP Build Flags
-PB_TORCH_PATH := /sys/class/flashlight_core/flashlight
+PB_TORCH_PATH := "/sys/class/flashlight_core/flashlight"
 PB_DISABLE_DEFAULT_TREBLE_COMP := true
 PB_DISABLE_DEFAULT_DM_VERITY := true
