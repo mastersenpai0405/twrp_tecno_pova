2e7bc21cb7347559639a9460b886a9c86f245633
Tata Lee <67614323+macukex1991@users.noreply.github.com>
2021-05-21 19:58:18 +0800

    Update omni_LD7.mk

--- omni_LD7.mk
+++ omni_LD7.mk
@@ -35,8 +35,7 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/roo
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+        fastbootd
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := LD7
